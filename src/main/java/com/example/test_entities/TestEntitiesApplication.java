f99603720e5b20608725c66486a422bae5dcd293
macdrien <adrien.bouyssou@protonmail.com>
2024-12-19 21:55:21 +0100

    Initial commit

---
+++ src/main/java/com/example/test_entities/TestEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEntitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_entities/TestEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
